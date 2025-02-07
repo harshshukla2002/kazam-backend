9edc7fe36efdb3de71b36b8f66599f458f8ecc37
harshshukla2002 <harshshukla2312@gmail.com>
2025-02-07 10:45:05 +0530

    Backend

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+
+import { connectDb } from "./config/db";
+import userRouter from "./routes/user.route";
+import taskRouter from "./routes/task.route";
+
+const server = express();
+
+server.use(express.json());
+server.use(cors());
+server.use("/api/user", userRouter);
+server.use("/api/task", taskRouter);
+
+server.get("/", (req, res) => {
+  res.json({ message: "server is running", success: true });
+});
+
+server.listen(process.env.PORT, async () => {
+  await connectDb();
+  console.log("server is running at http://localhost:4001/");
+});
